35cfe50fe91dd282c9262315b380d6619eedd4aa
oseevden <119258994+oseevden@users.noreply.github.com>
2022-12-13 22:18:38 +0300

    Add files via upload

---
+++ Step2/Program.cs
@@ -0,0 +1,41 @@
+
+//Задача 47
+
+int m = ReadInt("Введите количество строк: ");
+int n = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[m, n];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+// Заполнение массива рандомными вещественными числами
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+//  Функция вывода массива в терминал
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Функция ввода 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
